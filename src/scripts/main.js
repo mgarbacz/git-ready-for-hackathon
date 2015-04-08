2562e4ef6d0f2b4d8bf62db526ecfbf84afe6418
mgarbacz <mich.garbacz@gmail.com>
2015-04-07 22:23:00 -0500

    Add bash and git prism components

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -25,4 +25,6 @@ bespoke.from('article', [
 // This is actually loaded from "bower_components" thanks to
 // debowerify: https://github.com/eugeneware/debowerify
 require('prism');
+require('prism/components/prism-git')
+require('prism/components/prism-bash')
 
